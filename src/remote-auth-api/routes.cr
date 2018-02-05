982996c6d5322ec186a4e4c0023320e13499a30c
Peter Doherty <pdoherty926@gmail.com>
2018-02-05 12:23:45 -0500

    add heartbeat endpoint

--- src/remote-auth-api/routes.cr
+++ src/remote-auth-api/routes.cr
@@ -46,20 +46,21 @@ post "/users/auth/sign_in" do |env|
     auth = Types::UserAuth.from_json(env.params.json.to_json)
     user = USER_DATABASE[auth.email]
     if user.password == auth.password
-      success_response = Hash(String, Hash(String, Types::User)).new
-      success_response["data"] = {"attributes" => user}
+      success_response = SuccessResponse.new
+      success_response["data"] = {"attributes" => user.to_h}
       env.response.status_code = 200
       success_response.to_json
     else
-      env.response.status_code = 401
+      raise "Invalid email or password."
     end
-  rescue
+  rescue exception: Exception
+    error_response = ErrorResponse.new
+    error_response["errors"] = [exception.message.to_s]
     env.response.status_code = 401
+    error_response.to_json
   end
 end
 
-# get "/users/:email" do |env|
-#   env.response.content_type = "application/json"
-#   email = env.params.url["email"]
-#   USER_DATABASE[email].to_json
-# end
+get "/heartbeat" do |env|
+  env.response.status_code = 200
+end
